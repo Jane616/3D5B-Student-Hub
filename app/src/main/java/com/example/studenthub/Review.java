b648f10966a60736b72fc7dcbf22698c10a3d142
billy-mckenna <60261566+billy-mckenna@users.noreply.github.com>
2020-02-12 13:25:47 +0000

    Changed the review page so it starts with rating of zero and everytime you enter a rating and press submit rating it sets the rating to the average

--- app/src/main/java/com/example/studenthub/Review.java
+++ app/src/main/java/com/example/studenthub/Review.java
@@ -3,6 +3,7 @@ package com.example.studenthub;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.Toast;
 
@@ -10,7 +11,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Review extends AppCompatActivity {
     RatingBar ratingBar;
-    Button button;
+    Button checkButton;
+    EditText enterRatingText;
+    EditText ratingInfo;
+    Button submitRatingButton;
+    int rating_sum = 0;
+    int number_of_ratings = 0;
+    int average_rating = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +25,28 @@ public class Review extends AppCompatActivity {
         setContentView(R.layout.activity_review);
 
         ratingBar = (RatingBar) findViewById(R.id.ratingBar);
-        button = (Button) findViewById(R.id.button);
+        checkButton = (Button) findViewById(R.id.checkButton);
+        enterRatingText = (EditText) findViewById(R.id.enterRatingText);
+        submitRatingButton = (Button) findViewById(R.id.submitRatingButton);
+        ratingBar.setRating(0);
+        ratingInfo = (EditText) findViewById(R.id.ratingInfo);
+
+        submitRatingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String rating_value = enterRatingText.getText().toString();
+                Toast.makeText(Review.this, "Stars: " + rating_value, Toast.LENGTH_SHORT).show();
+                int star_value = Integer.parseInt(rating_value);
+                rating_sum = rating_sum + star_value;
+                number_of_ratings++;
+                average_rating = rating_sum / number_of_ratings;
+                ratingBar.setRating(average_rating);
+                ratingInfo.setText("Average Rating:" + average_rating);
+
+
+            }
+        });
+
 
         ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
@@ -27,7 +55,7 @@ public class Review extends AppCompatActivity {
             }
         });
 
-        button.setOnClickListener(new View.OnClickListener() {
+        checkButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(Review.this, "Stars: " + ratingBar.getRating(), Toast.LENGTH_SHORT).show();
