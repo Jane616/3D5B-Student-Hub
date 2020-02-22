807853f09f0f0b6f1821fb9e81e2006f3d66dc10
beerwithstraw <agarwalp@tcd.ie>
2020-02-22 03:25:29 +0000

    Realtime database update for attendance percentage

--- app/src/main/java/com/example/studenthub/Attendance.java
+++ app/src/main/java/com/example/studenthub/Attendance.java
@@ -6,62 +6,49 @@ import android.widget.Button;
 import android.widget.TextView;
 
 
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Attendance extends AppCompatActivity {
 
-    int numAttended = 13;
-    int numMissed = 5;
+    int numAttended = 0;
+    int numMissed = 0;
     TextView attendanceTextView;
     TextView moduleName;
-    Button buttonMissed;
-    Button buttonAttended;
+
+    TextView mAttendancePercentageTextView;
+    Button mButtonMissed;
+    Button mButtonAttended;
 
 
+    DatabaseReference RootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mPercentageRef = RootRef.child("percentage");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendance);
+        mAttendancePercentageTextView=(TextView)findViewById(R.id.attendancePercentageTextView);
+        mButtonMissed=(Button)findViewById(R.id.buttonMissed);
+        mButtonAttended=(Button)findViewById(R.id.buttonAttended);
+
 
         moduleName = findViewById(R.id.ModuleNameText);
         Bundle bn = getIntent().getExtras();
-        String modulename = bn.getString("modu  lename");
+        String modulename = bn.getString("modulename");
         moduleName.setText(String.valueOf(modulename));
 
-
-
-        int x = 5;
         System.out.println("Activity started");
         attendanceTextView = findViewById(R.id.attendancePercentageTextView);
 
     }
-    /*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-    */
 /*
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-    */
-
     public void onAttendedClick(View view) {
         System.out.println("On attended was clicked");
         numAttended ++;
@@ -80,12 +67,54 @@ public class Attendance extends AppCompatActivity {
     }
 
     private void updateAttendancePercentage() {
-        double attendedPercentage = 100 * numAttended / (numAttended + numMissed);
 
+        double attendedPercentage = 100 * numAttended / (numAttended + numMissed);
         String attendedPercentageAsString = String.valueOf(attendedPercentage);
+
 
         attendanceTextView.setText("Attendance: " + attendedPercentageAsString + "%");
 
+
+    }
+*/
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        mPercentageRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                mAttendancePercentageTextView.setText(text);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        mButtonAttended.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numAttended++;
+                double attendedPercentage = 100 * numAttended / (numAttended + numMissed);
+                String attendedPercentageAsString = String.valueOf(attendedPercentage);
+
+                mPercentageRef.setValue("Attendance: " + attendedPercentageAsString + "%");
+            }
+        });
+
+        mButtonMissed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numMissed++;
+                double attendedPercentage = 100 * numAttended / (numAttended + numMissed);
+                String attendedPercentageAsString = String.valueOf(attendedPercentage);
+                mPercentageRef.setValue("Attendance: " + attendedPercentageAsString + "%");
+
+            }
+        });
 
     }
 
