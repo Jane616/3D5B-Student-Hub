dc10a9f21ff391814b3b5b18b9613eb85d02d383
Saksham9399 <saagarwa@tcd.ie>
2020-02-25 01:14:30 +0000

    Created the basic signup page

---
+++ app/src/main/java/com/example/studenthub/SignUp.java
@@ -0,0 +1,20 @@
+package com.example.studenthub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class SignUp extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+
+
+    }
+}
