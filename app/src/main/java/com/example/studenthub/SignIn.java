76fa5a7cd00c72837e39ec013fa9facf504988cc
Jane616 <55512796+Jane616@users.noreply.github.com>
2020-03-04 19:22:47 +0000

    replace with saksham's version

--- app/src/main/java/com/example/studenthub/SignIn.java
+++ app/src/main/java/com/example/studenthub/SignIn.java
@@ -1,3 +1,4 @@
+/*
 package com.example.studenthub;
 
 import androidx.annotation.NonNull;
@@ -16,10 +17,50 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+*/
 
-public class SignIn extends AppCompatActivity {
+package com.example.studenthub;
 
-    private EditText mEmail;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
+public class SignIn extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        Spinner spinner= findViewById(R.id.year);
+        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(this,R.array.Year,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemClickListener((AdapterView.OnItemClickListener) this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text=parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),text,Toast.LENGTH_SHORT) .show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+    
+
+   /* private EditText mEmail;
     private EditText mPassword;
 
     private Button mSignUp;
@@ -111,4 +152,5 @@ public class SignIn extends AppCompatActivity {
         }
         return false;
     }
+    */
 }
