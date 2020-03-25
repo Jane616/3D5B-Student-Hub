2b59f6368d53b38716c09dbef672b5f0baa14e98
beerwithstraw <agarwalp@tcd.ie>
2020-03-25 16:44:13 +0530

    Student Solutions Page with Upload and Download PDF features. User can upload pdf for review and pdf can be downloaded by users from firebase storage.

---
+++ app/src/main/java/com/example/studenthub/StudentSolutions.java
@@ -0,0 +1,64 @@
+package com.example.studenthub;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class StudentSolutions extends AppCompatActivity {
+
+    TextView mTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_solutions);
+
+        mTextView = findViewById(R.id.uploadDoc);
+        String text = "Have a new solution? UPLOAD Here";
+        SpannableString ss = new SpannableString(text);
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+
+            }
+        };
+        ss.setSpan(clickableSpan, 21, 32, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        mTextView.setText(ss);
+        mTextView.setMovementMethod(LinkMovementMethod.getInstance());
+        mTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent upload = new Intent(StudentSolutions.this, StudentUpload.class);
+                startActivity(upload);
+            }
+        });
+
+    }
+
+    public void btn_action(View view) {
+        startActivity(new Intent(getApplicationContext(), SolutionsDownload.class));
+    }
+}
---
+++ app/src/main/java/com/example/studenthub/StudentUpload.java
@@ -0,0 +1,118 @@
+package com.example.studenthub;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class StudentUpload extends AppCompatActivity {
+
+    EditText editPDF;
+    Button mUpload;
+    Button mBack;
+    StorageReference storageReference;
+    DatabaseReference databaseReference;
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_upload);
+
+        editPDF = (EditText) findViewById(R.id.txt_PDF);
+        storageReference = FirebaseStorage.getInstance().getReference();
+        databaseReference = FirebaseDatabase.getInstance().getReference("uploads");
+        mUpload = (Button) findViewById(R.id.btn_upload);
+        mUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectPDFFile();
+
+            }
+        });
+        mBack = (Button) findViewById(R.id.backButton);
+        mBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent back = new Intent(StudentUpload.this, StudentSolutions.class);
+                startActivity(back);
+        }
+    });
+}
+
+
+
+
+
+
+    private void selectPDFFile() {
+        Intent intent = new Intent();
+        intent.setType("application/pdf");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select PDF File"), 1);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1 && resultCode == RESULT_OK
+                && data != null && data.getData()!=null){
+
+            uploadPDFFile(data.getData());
+        }
+    }
+
+    private void uploadPDFFile(Uri data) {
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.show();
+        progressDialog.setTitle("Uploading...");
+
+        StorageReference reference = storageReference.child("uploads/"+System.currentTimeMillis()+".pdf");
+        reference.putFile(data)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                        Task<Uri> uri = taskSnapshot.getStorage().getDownloadUrl();
+                        while(!uri.isComplete());
+                        Uri url = uri.getResult();
+
+                        uploadPDF uploadPDF = new uploadPDF(editPDF.getText().toString(), url.toString());
+                        databaseReference.child(databaseReference.push().getKey()).setValue(uploadPDF);
+                        Toast.makeText(StudentUpload.this, "File Uploaded", Toast.LENGTH_SHORT).show();
+                        progressDialog.dismiss();
+                    }
+                }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+
+                double progress = (100.0*taskSnapshot.getBytesTransferred())/taskSnapshot.getTotalByteCount();
+                progressDialog.setMessage("Uploaded: "+ (int)progress+ "%");
+
+
+            }
+        });
+
+    }
+}
