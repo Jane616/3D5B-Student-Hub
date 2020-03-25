c5134d544483f7cfbf28666a1e5910bd2115ce92
Ruijie Xiong <xiongr@tcd.ie>
2020-03-25 22:10:45 +0000

    connect current user with reminders page

--- app/src/main/java/com/example/studenthub/Attendance.java
+++ app/src/main/java/com/example/studenthub/Attendance.java
@@ -13,7 +13,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -216,7 +215,7 @@ public class Attendance extends AppCompatActivity {
                 startActivity(startIntent);
                 break;
             case R.id.menuReminders:
-                Intent otherIntent = new Intent(getApplicationContext(), Reminders1.class);
+                Intent otherIntent = new Intent(getApplicationContext(), AlarmPage.class);
                 startActivity(otherIntent);
                 break;
         }
