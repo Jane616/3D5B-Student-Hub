119b1e23dca4a2026906947bde0121a01341cef3
harsh <harsh_dhingra@hotmail.com>
2020-03-28 00:16:58 +0530

    resolved the error

--- app/src/main/java/com/example/studenthub/SignIn.java
+++ app/src/main/java/com/example/studenthub/SignIn.java
@@ -22,6 +22,7 @@ import com.google.firebase.auth.FirebaseAuth;
 package com.example.studenthub;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
